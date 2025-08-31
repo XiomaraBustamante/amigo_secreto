28b7b746b2add8d212fddfe7c73a820ef8fe2749
Xiomara <xio0809@gmail.com>
2025-08-30 20:23:12 -0500

    funciones amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//declarando un array para almacenar los nombres
+let nombres=[];
+console.log(nombres);
+
+//agregando una funcion para agregar los nombres
+function agregarAmigo(){
+    let nomAmigo=document.getElementById('amigo').value;
+    
+    //Si esta vacio pon un alert
+    if(nomAmigo==""){
+        alert('Porfavor ingrese un nombre');
+    }
+    //si no almacena los nombres en el arreglo
+    else{
+       nombres.push(nomAmigo);
+       limpiarCaja();
+    }
+}
+
+//recorrer el arreglo y crear el li
+function actualizarLista(){
+    let lista=document.getElementById('listaAmigos');
+}
+
+
+
+
+
+//limpiar el input
+function limpiarCaja(){
+    document.getElementById('amigo').value='';  
+}
